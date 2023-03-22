de800892c3ccb11fd6f0030bce8dc1d8d3ec1ddd
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-03-22 06:21:14 +0100

    new

---
+++ script.js
@@ -0,0 +1,30 @@
+// console.log("hello word")
+
+// alert("hello word")
+
+// console.log(typeof 4.5)
+
+// console.log(typeof true)
+
+// console.log(typeof 4 < 3)
+
+// console.log(typeof 4 > 2)
+
+
+
+// var myname=prompt ("enter a number", "");
+
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  
