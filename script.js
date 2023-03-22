db10c37d736c08b04ec6b8d217560e353581b016
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-03-22 15:24:48 +0100

    new

--- script.js
+++ script.js
@@ -16,15 +16,85 @@
 
 
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-  }
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
   
+
+// function greet (name) {
+//      return 'Hello world' +name;
+// }
+// console.log (greet('Peter'))
+
+// const color = "RED"
+
+// function changeColor ()  {
+//   return color
+// }
+// console.log(color ())
+
+
+// function isPrime(num) {
+//      // Check if num is less than 2, as all prime numbers are greater than 1
+//      if (num < 2) {
+//        return false;
+//      }
+         
+//      // Check for divisors from 2 up to the square root of num
+//      for (let i = 2; i <= Math.sqrt(num); i++) {
+//        if (num % i === 0) {
+//          return false;
+//        }
+//      }
+   
+//      // If no divisors were found, num is prime
+//      return true;
+//    }
+//    console.log(isPrime(7))
+ 
+
+// function isPrime(n){
+//      if (n===1){
+//           return false;
+//      }else if (n===2){
+//           return true
+//      }else{
+//           for (var x=2; x< n; x++){
+//                if (n % x === 0){
+//                     return false
+//                }
+//           }
+//           return true;
+//      }
+
+// }
+// console.log(isPrime(3))
+
+//    function getType(value) {
+//      return typeof value;
+//    }    
+       
+
+//    console.log(getType("hello")); // "string"
+//    console.log(getType(42)); // "number"
+//    console.log(getType(true)); // "boolean"
+//    console.log(getType({})); // "object"
+//    console.log(getType(undefined)); // "undefined"
+//    console.log(getType(function() {})); // "function"
+
+
+//    console.log(typeof true)
+//    console.log(typeof 42)
+//    console.log(typeof undefined)
+//    console.log(typeof Hello)
+//    console.log(typeof function() {})
+//    console.log(typeof {})
+   
